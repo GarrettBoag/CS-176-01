5e9d3f2bea68670e24fb46a9d6b3a076086be507
GarrettBoag <146123631+GarrettBoag@users.noreply.github.com>
2024-10-24 18:11:36 -0400

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,24 @@
+package edu.monmouth.lab1;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// solution to problem 1
+		
+		// solution to problem 2
+		
+		// solution to problem 3
+		
+		// solution to problem 4
+		
+		// solution to problem 5
+
+		// solution to problem 5
+
+		// solution to problem 6
+		
+		// solution to extra credit
+
+	}
+
+}
